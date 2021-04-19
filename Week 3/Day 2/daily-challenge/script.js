cbd07f17c73d9ec64e13380dd2e6ba98b6db2338
Marcos Vinicius <mvsouza7@gmail.com>
2021-04-19 20:02:49 +0300

    added Day 2 daily challenge and exercises xp folder and files

---
+++ Week 3/Day 2/daily-challenge/script.js
@@ -0,0 +1,40 @@
+// Exercise 1
+// Using this array :
+
+// let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+// 1- Remove Banana from the array.
+// let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+// fruits.shift();
+// console.log(fruits);
+
+//------------------------------------------------------------
+
+// 2- Sort the array in alphabetical order. 
+// let fruits = ["Apples", "Oranges", "Blueberries"];
+// console.log(fruits.sort());
+//------------------------------------------------------------
+
+// 3- Add “Kiwi” to the end of the array.
+// let fruits = ["Apples", "Oranges", "Blueberries"];
+// fruits.push("Kiwi");
+// console.log(fruits);
+
+//------------------------------------------------------------
+// 4- Remove “Apples” from the array. Don’t use the same method as in part 1.
+// let fruits = ["Apples", "Oranges", "Blueberries", "Kiwi"];
+// fruits.splice(0,1);
+// console.log(fruits);
+//------------------------------------------------------------
+
+// 5- Sort the array in reverse order. (Not alphabetical, but reverse the current Array i.e. [‘a’, ‘c’, ‘b’] becomes [‘b’, ‘c’, ‘a’])
+// let fruits = ["Blueberries", "Oranges", "Kiwi"];
+// fruits.reverse();
+// console.log(fruits);
+//------------------------------------------------------------
+
+
+//Exercise 2
+// Acess array element
+// let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+// console.log(moreFruits[1][1][0]);
