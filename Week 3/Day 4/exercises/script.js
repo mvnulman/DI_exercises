62d899b7937b7c9cf69fc3cc2de2c28f11e8f2f6
Marcos Vinicius <mvsouza7@gmail.com>
2021-04-21 22:46:07 +0300

    added day 4 / exercises folders with index.html and script.js

---
+++ Week 3/Day 4/exercises/script.js
@@ -0,0 +1,29 @@
+// let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"]
+// console.log(users);
+
+// Using the array above, console.log the number of users in a group chat based on the following rules:
+
+// If there is no users (the users array is empty), console.log “no one is online”.
+// if (users.keys = []) {
+//     console.log('no one is online');
+    
+// }
+
+// If there is 1 user, console.log “<name_user> is online”.
+// if (users.keys = 1) {
+//     console.log(`Lea_${users[1]} is online`);
+
+// }
+
+
+// // If there are 2 users, console.log “<name_user1> and <name_user2> are online”.
+// if (users.keys = [2]) {
+//     console.log(`${users[1]} and ${users[2]} are online`);
+
+// }
+
+// If there are more than 2 users, console.log the first two names in the users array and the number of additional users online.
+// if (users.keys > [2]) {
+//     console.log(`${users[1]} , ${users[2]} and other 2 users are online`);
+
+// }
