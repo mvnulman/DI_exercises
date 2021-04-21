489f8b45ed4480ac52c447a4523d57f8dc58d788
Marcos Vinicius <mvsouza7@gmail.com>
2021-04-22 00:44:33 +0300

    exercise update

--- Week 3/Day 4/exercises/script.js
+++ Week 3/Day 4/exercises/script.js
@@ -43,54 +43,50 @@
 // }
 
 
+// ---------------------------------------------
+// Exercise 3: Even Or Odd
+// ---------------------------------------------
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Instructions
+// Prompt the user for a number and save it to a variable.
+// let number = parseInt( prompt("Please enter a number:"));
+// // If it is even, display: “x is an even number”. Where x is the actual number the user chose.
+// if (number % 2 == 0) {
+// 	console.log(`${number} is an even number!`);
+// // If it is odd, display: “x is an odd number”. Where x is the actual number the user chose.
+// } else {
+//     console.log(`${number} is an odd number!`)};
 
 
 // ---------------------------------------------
 // Exercise 4: 
 // ---------------------------------------------
-// let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"]
-// console.log(users);
+let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"]
+console.log(users);
 
 // Using the array above, console.log the number of users in a group chat based on the following rules:
 
 // If there is no users (the users array is empty), console.log “no one is online”.
-// if (users.keys = []) {
-//     console.log('no one is online');
+if (users.lenght == 0) {
+    console.log('no one is online');
 
-// }
+}
 
 // If there is 1 user, console.log “<name_user> is online”.
-// if (users.keys = 1) {
-//     console.log(`Lea_${users[1]} is online`);
-
-// }
+else if (users.length == 1) 
+{
+	console.log(`${users[0]} is online`);
+}
 
 
 // // If there are 2 users, console.log “<name_user1> and <name_user2> are online”.
-// if (users.keys = [2]) {
-//     console.log(`${users[1]} and ${users[2]} are online`);
-
-// }
+else if (users.length == 2) 
+{
+	console.log(`${users[0]} and ${users[1]} are online`);
+}
 
 // If there are more than 2 users, console.log the first two names in the users array and the number of additional users online.
-// if (users.keys > [2]) {
-//     console.log(`${users[1]} , ${users[2]} and other 2 users are online`);
-
-// }
+else if (users.length > 2) 
+{
+	console.log(`${users[0]} , ${users[1]} and ${users.length = users.length - 2} more are online`);
+}
