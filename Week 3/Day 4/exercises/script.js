3051ac3c9b514459228119e3d2b509fbd31795b3
Marcos Vinicius <mvsouza7@gmail.com>
2021-04-22 00:17:32 +0300

    adjustments on scripts.js exercises

--- Week 3/Day 4/exercises/script.js
+++ Week 3/Day 4/exercises/script.js
@@ -1,3 +1,70 @@
+// ---------------------------------------------
+// // Exercise 1: Simple If/Else Statement
+// ---------------------------------------------
+// // Instructions
+// // Create 2 variables, x and y. Each of them should have a different numeric value.
+// // Write an if/else statement that checks which number is bigger.
+// // Example :
+
+// let x = 5;
+// let y = 2;
+
+// if (x > y){
+//     console.log("X is the biggest number")
+// }else{
+//     console.log("Y is bigger");
+// }
+
+// // // You should display:
+// // // x is the biggest number
+
+
+// ---------------------------------------------
+// Exercise 2: Chihuahua
+// ---------------------------------------------
+// Instructions
+// Create a variable called newDog where it’s value is “Chihuahua”.
+// let newDog = "Chihuahua";
+//     console.log(newDog);
+// // Check and display how many letters are in newDog.
+//    console.log("there are ", newDog.length, "characters in newdog.");
+// // Display the newDog variable in uppercase and then in lowercase (no need to create new variables, just console.log twice).
+//     console.log(newDog.toUpperCase());
+//     console.log(newDog.toLowerCase());
+// // Check if the variable newDog is equal to “Chihuahua”
+//     console.log(newDog == "Chihuahua");
+// // if it does, display ‘I love Chihuahuas, it’s my favorite dog breed’
+// if (newDog == "Cats") {
+//     console.log("I love Chihuahuas, it’s my favorite dog breed");
+
+//     // else, console.log ‘I dont care, I prefer cats’
+// } else {
+//     console.log ("I dont care, I prefer cats");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ---------------------------------------------
+// Exercise 4: 
+// ---------------------------------------------
 // let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"]
 // console.log(users);
 
@@ -6,7 +73,7 @@
 // If there is no users (the users array is empty), console.log “no one is online”.
 // if (users.keys = []) {
 //     console.log('no one is online');
-    
+
 // }
 
 // If there is 1 user, console.log “<name_user> is online”.
