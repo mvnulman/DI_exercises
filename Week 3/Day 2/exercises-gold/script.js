a149228712f5877b527016b12cf094aafd25c667
Marcos Vinicius <mvsouza7@gmail.com>
2021-04-25 17:31:39 +0300

    Added week 3, day 2, exercises gold (with html and javascrip files

---
+++ Week 3/Day 2/exercises-gold/script.js
@@ -0,0 +1,45 @@
+// ---------------------------------
+// Exercise 1 : Favorite Color
+//----------------------------------
+// Instructions
+// let colors = ["my","favorite","color","is","blue"]
+
+// Write some simple Javascript code that will join all the items in the array above, and console.log the result.
+// console.log(colors.join(' '));
+
+// ---------------------------------
+// Exercise 2 : Mixup
+//----------------------------------
+// Instructions
+// Create 2 variables. The values should be strings. For example:
+// let str1 = "mix";
+// let str2 = "pod";
+
+// // Slice out and swap the first 2 characters of the 2 strings from part 1.
+// // Create a third variable where the value is the concatenation of the two strings from the part 1 (separated by a space).
+// let newWord = `${str2.slice(0,2)}${str1.slice(2)} ${str1.slice(0,2)}${str2.slice(2)}`
+
+// // Finally console.log the new concatenated string.
+// console.log(newWord);
+
+// ---------------------------------
+// Exercise 3 : Calculator
+//----------------------------------
+// Instructions
+// Make a Calculator. Follow the instructions:
+
+// // Prompt the user for the first number.
+// // Store the first number in a variable called num1.
+// let num1 = parseFloat(prompt("Please enter the first number: "));
+// console.log(num1);
+
+// // Prompt the user for the second number.
+// // Store the second number in a variable called num2.
+// let num2 = parseFloat(prompt("+ the second number: "));
+// console.log(num2);
+
+// // Create an Alert where the value is the SUM of num1 and num2.
+// let result = (num1 + num2);
+// alert(`Your number is ${result}`);
+// console.log(result);
+// // BONUS: Make a program that can subtract, multiply, and also divide
