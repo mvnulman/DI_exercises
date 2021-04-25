32579347ab411dd041bdd575b7b23420b3042555
Marcos Vinicius <mvsouza7@gmail.com>
2021-04-25 15:32:10 +0300

    script.js of daily challenge updated

--- Week 3/Day 5/daily-challenge/script.js
+++ Week 3/Day 5/daily-challenge/script.js
@@ -1,11 +1,16 @@
-var i, j;
-//outer loop
-for(i=1; i <= 6; i++)
- {
- //inner loop
-  for(j=1; j<=i; j++)
- {
-   console.log('*');
-  }
-   console.log('<br/>');
- }
+// Write a JavaScript program that recreates the pattern below.
+
+// Do this challenge twice: first by using one loop, 
+// let text =""
+// for (let index = 0; index < 6; index++) {
+//     text+="*"
+//     console.log(text)
+// }
+// // then by using two nested for loops (Nested means one inside the other - check out “nested for loops” on Google).
+// for (let i = 0; i < 6; i++) {
+//     let text =""
+//     for (let j = 0; j <=i; j++) {
+//         text+="*"
+//     };
+//     console.log(text)
+// }
