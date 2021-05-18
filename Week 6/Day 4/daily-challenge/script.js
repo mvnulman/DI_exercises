8b8f006680031315adc8697fd908ef23ee0444ff
Marcos Vinicius <mvsouza7@gmail.com>
2021-05-18 15:41:14 +0300

    js file fix

--- Week 6/Day 4/daily-challenge/script.js
+++ Week 6/Day 4/daily-challenge/script.js
@@ -1,17 +1,18 @@
 let groceries = {
-    fruits: ["pear", "apple", "banana"],
+    fruits : ["pear", "apple", "banana"],
     vegetables: ["tomatoes", "cucumber", "salad"],
-    totalPrice: "20$",
-    other: {
-        payed: true,
-        meansOfPayment: ["cash", "creditCard"]
+    totalPrice : "20$",
+    other : {
+        payed : true,
+        meansOfPayment : ["cash", "creditCard"]
     }
 }
 
-let clone = Object.assign({}, groceries);
-groceries.totalPrice = "35$";   //we will not see the modifications, because it's another object
-groceries.other.payed = false;  //we will see the modifications, because it's the same object, but inside two different objects
+let objJson = JSON.stringify(groceries);
+let groceries2 = JSON.parse(objJson);
 
+groceries2.totalPrice = "35$";
 
-console.log(clone);
-console.log(groceries);
+let payed = groceries.other.payed = false
+console.log(payed) // --> it changed only locally, not in the parent obj
+console.log(groceries2) // --> it doesn't change PAYED: true inside this object because "other" is also a nested object
