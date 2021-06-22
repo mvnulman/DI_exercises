50162493d282c6bc393087ca66d8d3a70cf4f66d
Marcos Vinicius <mvsouza7@gmail.com>
2021-06-22 23:28:48 +0300

    Week 12, day 2 - xp exercises

---
+++ Week 12/Day 2/xp-exercises/npm-node/main.js
@@ -0,0 +1,47 @@
+//----------------------------
+// Exercise 1 : Hello Node.Js
+//----------------------------
+// Create a new folder called - npm-node
+// Create a JS file called - main.js
+// Add the following code to the file main.js :
+
+let a = 5;
+let b = 10;
+let sum = a+b;
+
+console.log(`Hello you are ${sum}`)
+
+
+//------------------------------
+// Exercise 2 : Hello Node.Js #2
+//------------------------------
+// Create a package.json file - use npm init
+// Run the main.js file using npm start.
+// Tip: you need to change the package.json file in order to achieve this.
+// You should output something in the command prompt, for example: Hi, thanks for running me with npm start!
+
+console.log('Hi, thanks for running me with npm start!')
+
+//------------------------------
+// Exercise 3 : Nodemon
+//------------------------------
+// Create a package.json file - use npm init
+// Install nodemon
+// Write the following code, in a main.js file:
+
+let aa = 5;
+let bb = 10;
+let sum1 = aa+bb
+let x;
+let c = 6;
+x = aa + bb;
+// let sum2 = c + ;
+
+
+// Add some more code in the main.js so when you run it using nodemon, it logs the sum of a + b as a first run.
+// Tip: you need to change the package.json file in order to achieve this.
+
+// Update the main.js file to console.log the sum of x + c
+
+console.log(`First Run: ${sum1}`);
+console.log(`Second Run: ${x+c}`);
