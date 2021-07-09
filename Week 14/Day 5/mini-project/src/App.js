46ee6e2674e467a57a7a21e58942a165572beaa9
Marcos Vinicius <mvsouza7@gmail.com>
2021-07-09 16:45:40 +0300

    update files

--- Week 14/Day 5/mini-project/src/App.js
+++ Week 14/Day 5/mini-project/src/App.js
@@ -1,8 +1,17 @@
 import React, { useState, useEffect} from "react";
 import "./App.css";
 
+
 const App = () => {
   const [quotes, setQuotes] = useState ('');
+
+  const randomColor = () => {
+      const color = `rgb(
+      ${Math.floor(Math.random() * 155)},
+      ${Math.floor(Math.random() * 155)},
+      ${Math.floor(Math.random() * 155)})`;
+      return color;
+  }
 
   const getQuote = () => {
     fetch("https://type.fit/api/quotes")
@@ -10,7 +19,10 @@ const App = () => {
     .then((data) => {
       const randomNum = Math.floor(Math.random() * data.length);
       setQuotes(data[randomNum]);
+      
     });
+    randomColor()
+
   };
 
   useEffect(() => {
@@ -22,10 +34,11 @@ const App = () => {
         <div className = "quote-container">
         <h1>"{quotes.text}"</h1>
         <h2>{quotes.author}</h2><br/>
-        <button className ="btn" onClick = {getQuote}> New quote</button>
+        <button className ="btn" id="btn" onClick = {getQuote}> New quote</button>
         </div>
     </div>
     );
 };
+
 
 export default App;
