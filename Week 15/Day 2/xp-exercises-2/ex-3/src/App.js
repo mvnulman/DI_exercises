c84eb7372ca0461bca92a8b9487de2ce1cf227c1
Marcos Vinicius <mvsouza7@gmail.com>
2021-07-12 17:12:06 +0300

    Week 15, Day 2 - xp exercises 2

--- Week 15/Day 2/xp-exercises-2/ex-3/src/App.js
+++ Week 15/Day 2/xp-exercises-2/ex-3/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+
+class Child extends React.Component {
+  componentWillUnmount() {
+    alert("The component is about to be unmounted");
+  }
+  render() {
+    return (
+      <React.Fragment>
+      <h3>Hello World!</h3>
+      </React.Fragment>
+    );
+  }
+}
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      favoriteColor: 'red',
+      show: true
+    }
+  }
+
+  delete = () => {
+    this.setState({show:false});
+  }
+  
+  render() {
+    var example;
+    if (this.state.show) {
+      example = <Child />
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {example}
+      <button onClick={this.delete}>Delete Example</button>
+    </>
+  )
+}
 }
 
 export default App;
